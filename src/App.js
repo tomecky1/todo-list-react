41ac1c60a5280985f0ba0eabb74ee574a0c792b0
Tomasz Piotrowski <tpiotrowski007@gmail.com>
2024-01-20 18:27:29 +0100

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -38,12 +38,23 @@ function App() {
     })))
   };
 
+  const addNewTask = (newTaskContent) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content: newTaskContent,
+        done: false,
+        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
   return (
     <Container>
       <Header />
       <Section
         title="Dodaj nowe zadanie"
-        children={<Form />}
+        children={<Form addNewTask={addNewTask} />}
       />
       <Section
         title="Lista zadań:"
@@ -52,6 +63,6 @@ function App() {
       />
     </Container>
   );
-}
+};
 
 export default App;
